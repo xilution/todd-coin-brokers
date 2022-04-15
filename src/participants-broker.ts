4b18d5d14a17738a62c8c5d8521db9e061900f7b
Todd Brunia <tbrunia@gmail.com>
2022-04-15 09:56:23 -0500

    fix: improved error response

--- src/participants-broker.ts
+++ src/participants-broker.ts
@@ -56,7 +56,7 @@ export const getParticipantByPublicKey = async (
 
   if (models.length > 1) {
     throw new Error(
-      `unable to get a participant by public key b/c more than one participant was found for the public key ${publicKey}`
+      `unable to get a participant by public key because more than one participant was found for the public key ${publicKey}`
     );
   }
 
